898f0640c1aded3bf7fa8259c653bcff74a373b2
Victor Ortega <victormatiasortega@gmail.com>
2014-04-19 14:07:18 -0300

    added empty service

---
+++ app/src/main/java/com/vortega/exercise_2/app/MLService.java
@@ -0,0 +1,16 @@
+package com.vortega.exercise_2.app;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class MLService extends Service {
+    public MLService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
